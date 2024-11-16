11dfcf4e3127e87b718d5e765fc1fa538dc44649
Asger Finding <asger.finding@gmail.com>
2024-11-16 15:17:39 +0100

    feat: add switch logic between default and classic mouse

--- src/scripts/content/classicMouse.js
+++ src/scripts/content/classicMouse.js
@@ -1,3 +1,25 @@
+import { StoreEvent, get, onStateChange } from '../common/store.js';
+
+let isClassicMouse = false;
+
+/**
+ * Set the classic mouse option
+ * @param option Is classic mouse enabled?
+ */
+const setClassicMouse = option => {
+	isClassicMouse = option === true;
+};
+
+// Initialize classicMouse setting
+get('classicMouse').then(setClassicMouse);
+
+// Handle state changes
+onStateChange(({ detail }) => {
+	if (
+		detail?.type === StoreEvent.STORE_CHANGE &&
+		typeof detail.data?.curr?.classicMouse !== 'undefined'
+	) setClassicMouse(detail.data.curr.classicMouse);
+});
 
 UIConstants.classField('CLASSIC_MOUSE_INPUT', {
 	ROTATION_DEAD_ANGLE: 0.1,
@@ -9,56 +31,96 @@ MouseInputManager.method('update', function() {
 	this._super();
 
 	const game = GameManager.getGame();
-	if (game) {
-		let forwardState = false;
-		const backState = false;
-		const leftState = [];
-		const rightState = [];
-		let fireState = false;
+	if (!game || !game.state.getCurrentState().getTankSprite) return;
 
-		const gameBounds = game.scale.bounds;
-		const gameScale = game.scale.scaleFactor;
-		this.mouseX = (MouseInputManager.mousePageX - gameBounds.x) * gameScale.x;
-		this.mouseY = (MouseInputManager.mousePageY - gameBounds.y) * gameScale.y;
-		if (game.input.enabled && MouseInputManager.mouseActivated) {
-			if (game.state.getCurrentState().getTankSprite) {
-				const tankSprite = game.state.getCurrentState().getTankSprite(this.playerId);
-				if (tankSprite) {
-					const relativeToTank = tankSprite.toLocal(new Phaser.Point(this.mouseX, this.mouseY));
-					const angle = Phaser.Math.angleBetween(0, 0, relativeToTank.x, relativeToTank.y);
-					const distance = Math.abs((angle / Math.PI) + 0.5);
+	const gameBounds = game.scale.bounds;
+	const gameScale = game.scale.scaleFactor;
+	this.mouseX = (MouseInputManager.mousePageX - gameBounds.x) * gameScale.x;
+	this.mouseY = (MouseInputManager.mousePageY - gameBounds.y) * gameScale.y;
 
-					const rotationSpeedMultiplier = distance > 0.1
-						? Math.max(Math.min(distance * 12, 4), 1)
-						: 1;
+	if (!game.input.enabled || !MouseInputManager.mouseActivated) return;
 
-					leftState.push(angle + (Math.PI * 0.5) < -UIConstants.CLASSIC_MOUSE_INPUT.ROTATION_DEAD_ANGLE, rotationSpeedMultiplier);
-					rightState.push(angle + (Math.PI * 0.5) > UIConstants.CLASSIC_MOUSE_INPUT.ROTATION_DEAD_ANGLE, rotationSpeedMultiplier);
+	let forwardState = false;
+	let backState = false;
+	let leftState = [false, 1.0];
+	let rightState = [false, 1.0];
+	let fireState = false;
 
-					forwardState = relativeToTank.getMagnitude() > UIConstants.CLASSIC_MOUSE_INPUT.POSITION_DEAD_DISTANCE / UIConstants.GAME_ASSET_SCALE;
+	const tankSprite = game.state.getCurrentState().getTankSprite(this.playerId);
+	if (!tankSprite) return;
 
-					fireState ||= KeyboardInputManager.leftMouseDown || game.input.mousePointer.leftButton.isDown;
-				}
+	const relativeToTank = tankSprite.toLocal(new Phaser.Point(this.mouseX, this.mouseY));
+	const magnitude = relativeToTank.getMagnitude();
+	let angle = Phaser.Math.angleBetween(0, 0, relativeToTank.x, relativeToTank.y);
+	if (angle > Math.PI / 2) angle = -angle;
+
+	if (isClassicMouse) {
+		const distance = Math.abs((angle / Math.PI) + 0.5);
+		const rotationMultiplier = distance > 0.1
+			? Math.max(Math.min(distance * 12, 4), 1)
+			: 1;
+
+		rightState = [angle + (Math.PI * 0.5) > UIConstants.CLASSIC_MOUSE_INPUT.ROTATION_DEAD_ANGLE, rotationMultiplier];
+		leftState = [angle + (Math.PI * 0.5) < -UIConstants.CLASSIC_MOUSE_INPUT.ROTATION_DEAD_ANGLE, rotationMultiplier];
+
+		forwardState = magnitude > UIConstants.CLASSIC_MOUSE_INPUT.POSITION_DEAD_DISTANCE / UIConstants.GAME_ASSET_SCALE;
+		backState = false;
+	} else {
+		const canReverse = magnitude < UIConstants.MOUSE_INPUT.MAX_REVERSE_DISTANCE / UIConstants.GAME_ASSET_SCALE;
+		let goInReverse = false;
+		if (angle > Math.PI * 0.5 + UIConstants.MOUSE_INPUT.ROTATION_DEAD_ANGLE || angle < -Math.PI * 0.5 - UIConstants.MOUSE_INPUT.ROTATION_DEAD_ANGLE) {
+			if (angle > 0 && canReverse) {
+				rightState = [true, 1.0];
+				goInReverse = true;
+			} else {
+				leftState = [true, 1.0];
+			}
+		} else if (angle > -Math.PI * 0.5 + UIConstants.MOUSE_INPUT.ROTATION_DEAD_ANGLE && angle < Math.PI * 0.5 - UIConstants.MOUSE_INPUT.ROTATION_DEAD_ANGLE) {
+			if (angle > 0 && canReverse) {
+				leftState = [true, 1.0];
+				goInReverse = true;
+			} else {
+				rightState = [true, 1.0];
+			}
+		} else if (angle > 0) {
+			if (canReverse)
+				goInReverse = true;
+			else if (angle > Math.PI * 0.5)
+				leftState = [true, 1.0];
+			else
+				rightState = [true, 1.0];
+
+		}
+		if (magnitude > UIConstants.MOUSE_INPUT.POSITION_DEAD_DISTANCE / UIConstants.GAME_ASSET_SCALE) {
+			if (canReverse) {
+				forwardState = !goInReverse;
+				backState = goInReverse;
+			} else if (angle > -Math.PI * 0.5 - UIConstants.MOUSE_INPUT.POSITION_DEAD_ANGLE && angle < -Math.PI * 0.5 + UIConstants.MOUSE_INPUT.POSITION_DEAD_ANGLE) {
+				forwardState = true;
 			}
 		}
-		let stateChanged = false;
-		stateChanged ||= this.storedStates.forward !== forwardState;
-		stateChanged ||= this.storedStates.fire !== fireState;
-		stateChanged ||= !leftState.every((el, i) => this.storedStates.left[i] === el);
-		stateChanged ||= !rightState.every((el, i) => this.storedStates.right[i] === el);
-
-		const gameController = GameManager.getGameController();
-		if (stateChanged && gameController) {
-			const inputState = InputState.withState(this.playerId, forwardState, backState, leftState, rightState, fireState);
-			gameController.setInputState(inputState);
-		}
-
-		this.storedStates.forward = forwardState;
-		this.storedStates.back = backState;
-		this.storedStates.left = leftState;
-		this.storedStates.right = rightState;
-		this.storedStates.fire = fireState;
 	}
+
+	fireState ||= MouseInputManager.mouseDown || game.input.mousePointer.leftButton.isDown;
+
+	let stateChanged = false;
+	stateChanged ||= this.storedStates.forward !== forwardState;
+	stateChanged ||= this.storedStates.back !== backState;
+	stateChanged ||= this.storedStates.fire !== fireState;
+	stateChanged ||= !leftState.every((el, i) => this.storedStates.left[i] === el);
+	stateChanged ||= !rightState.every((el, i) => this.storedStates.right[i] === el);
+
+	const gameController = GameManager.getGameController();
+	if (stateChanged && gameController) {
+		const inputState = InputState.withState(this.playerId, forwardState, backState, leftState, rightState, fireState);
+		gameController.setInputState(inputState);
+	}
+
+	this.storedStates.forward = forwardState;
+	this.storedStates.back = backState;
+	this.storedStates.left = leftState;
+	this.storedStates.right = rightState;
+	this.storedStates.fire = fireState;
 });
 
 Tank.method('setTankState', function(tankState) {

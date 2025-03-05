dd19c9f2dcdb2ece296b6e57258acf53147b6f4f
Asger Finding <asger.finding@gmail.com>
2025-03-05 08:49:44 +0100

    fix: input state not updating in online games with normal mouse input

--- src/scripts/content/classicMouse.js
+++ src/scripts/content/classicMouse.js
@@ -109,8 +109,8 @@ MouseInputManager.method('update', function() {
 	stateChanged ||= this.storedStates.fire !== fireState;
 	stateChanged ||= !leftState.every((el, i) => this.storedStates.left[i] === el);
 	stateChanged ||= !rightState.every((el, i) => this.storedStates.right[i] === el);
-	stateChanged ||= leftState[1] < 0.8 || leftState[1] > 1.2;
-	stateChanged ||= rightState[1] < 0.8 || rightState[1] > 1.2;
+	stateChanged ||= leftState[1] < 0.8 || leftState[1] > 1.2 || !isClassicMouse;
+	stateChanged ||= rightState[1] < 0.8 || rightState[1] > 1.2 || !isClassicMouse;
 
 	const gameController = GameManager.getGameController();
 	if (stateChanged && gameController) {
